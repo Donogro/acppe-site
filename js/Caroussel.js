9ecfd14bf165e3eb4d8339ed8a86449c5625d163
Donogro <38463727+Donogro@users.noreply.github.com>
2018-06-28 17:54:58 +0200

    Add files via upload

--- js/Caroussel.js
+++ js/Caroussel.js
@@ -1,22 +1,13 @@
-var slideIndex = 1;
+var slideIndex = 0;
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("bannieres");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("bannieres");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-} 
